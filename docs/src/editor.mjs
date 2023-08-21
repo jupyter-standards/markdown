a00af70f6731de0d97e888f8295af1e02ee8d946
Frédéric Collonval <fcollonval@gmail.com>
2023-08-21 14:58:56 +0200

    First working version of the live editor

--- docs/src/editor.mjs
+++ docs/src/editor.mjs
@@ -1,38 +1,57 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import {EditorView, basicSetup} from "codemirror"
-import {ViewPlugin} from "@codemirror/view"
-import {render} from '@jupyter/markdown'
-import {jupyterMarkdown} from '@jupyter/markdown-codemirror'
+import { EditorView, basicSetup } from "codemirror"
+import { ViewPlugin } from "@codemirror/view"
+import { render } from '@jupyter/markdown'
+import { jupyterMarkdown } from '@jupyter/markdown-codemirror'
 
-const editorSelector = '#editor';
-const renderedSelector = '#rendered'
+const EDITOR_SELECTOR = '#editor';
+const RENDERED_SELECTOR = '#preview';
+const RAW_SELECTOR = '#raw';
+const DEFAULT_MARKDOWN_EXAMPLE = `# Title
+
+This is a Markdown **paragraph** with _markup_.`;
 
 // Sync extension
 const syncRenderer = ViewPlugin.fromClass(class {
+  #raw;
   #renderer;
-  #renderInProgress = false;
+  #renderCounter = 0;
 
   constructor(view) {
-    this.#renderer = document.querySelector(renderedSelector);
-    this.update({docChanged: true, state: view.state});
+    this.#raw = document.querySelector(RAW_SELECTOR);
+    this.#renderer = document.querySelector(RENDERED_SELECTOR);
+    this.update({ docChanged: true, state: view.state });
   }
 
   update(update) {
-    if(update.docChanged) {
-      this.#renderInProgress = true;
-      render(update.state.doc.toString()).then(result => {if(!this.#renderInProgress){this.#renderer.innerHTML = result}});
+    if (update.docChanged) {
+      this.#renderCounter++;
+      const counter = this.#renderCounter;
+      this.#renderer.innerHTML = '<jp-progress-ring>Rendering Markdown...</jp-progress-ring>';
+      render(update.state.doc.toString()).then(result => { if (counter == this.#renderCounter) { this.#renderer.innerHTML = result; this.#raw.textContent = result } });
     }
   }
 })
 
-const editor = jupyterMarkdown().then(markdown => new EditorView({
-  doc: `# Title
+// Ensure to keep an handle on the editor to avoid garbage collection.
+let editor;
 
-This is a Markdown **paragraph** with _markup_.`,
-  extensions: [basicSetup, markdown, 
-    syncRenderer
-  ],
-  parent: document.querySelector(editorSelector)
-}));
+async function onLoad() {
+  const markdown = await jupyterMarkdown()
+
+  const queryArgs = new URLSearchParams(location.search);
+
+  editor = new EditorView({
+    doc: queryArgs.get('text') ?? DEFAULT_MARKDOWN_EXAMPLE,
+    extensions: [
+      basicSetup,
+      markdown,
+      syncRenderer
+    ],
+    parent: document.querySelector(EDITOR_SELECTOR)
+  });
+}
+
+window.addEventListener('load', onLoad);
